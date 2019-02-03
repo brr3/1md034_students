a7265d3347244ad50f5061411e97bf35e0105dc6
BuildTools <brunorees@yahoo.com>
2019-02-03 17:46:29 +0100

    Added menu in JSON format

---
+++ public/js/menu.js
@@ -0,0 +1,20 @@
+var menuItems = [
+    {
+        "name": "The Fire Burger",
+        "image": "https://www.colourbox.com/preview/4409422-hamburger-with-fire.jpg",
+        "kCal": 750,
+        "intolerences": ["Lactose", "Gluten"]
+    },
+    {
+        "name": "Fried Turkey Burger",
+        "image": "https://cdn0.wideopeneats.com/wp-content/uploads/2017/10/arbys-deep-fried-turkey-sandwiches.png",
+        "kCal": 600,
+        "intolerences": ["Lactose"]
+    },
+    {
+        "name": "A Double w/ Cheese",
+        "image": "https://www.mcdonalds.com.my/storage/foods/September2017/thumb2doublecheeseburger-efc3a63a9d.jpg",
+        "kCal": 1800,
+        "intolerences": ["Lactose", "Gluten"]
+    }
+]
